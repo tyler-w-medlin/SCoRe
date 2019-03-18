57c6dffb7e7b3ca3dd7f2af7147ad07f361d7647
John Zickafoose <johnzick25@gmail.com>
2019-03-17 20:28:41 -0500

    doin them things

--- ui/score.js
+++ ui/score.js
@@ -1,15 +1,11 @@
 function sendData(){
-    var XHR = new XMLHttpRequest();
-    var urlEncodedData = "";
-    var urlEncodedDataPairs = [];
-    var name;
-    var data = document.getElementById("searchbar").value;
-
-    /*for(name in data){
-        urlEncodedDataPairs.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]));
-    }*/
-
-    //urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+    var XHR = new XMLHttpRequest({mozSystem: true});
+    var data = { "search" : document.getElementById("searchbar").value };
+    var results = document.getElementById("results");
+    var searchResults;
+    var displayString = "";
+    var resultNum;
+    var theNode;
 
     XHR.addEventListener('load', (event)=> {
         console.log("Data sent and response loaded");
@@ -19,15 +15,33 @@ function sendData(){
         alert('Search not sent. Something went wrong.')
     })
 
-    /*for(entry in urlEncodedData){
-        console.log(entry);
-    }*/
+    XHR.onreadystatechange = ()=> {
+        if(XHR.readyState == 4){
+            console.log(XHR.response);
+            searchResults = JSON.parse(XHR.response);
+            //console.log(searchResults);
+            resultNum = 0;
+            for (var property in searchResults) {
+                if(searchResults.hasOwnProperty(property)) {
+                    displayString = displayString + "Result Number: " + resultNum + "\n";
+                    displayString = displayString + searchResults[property]["keywords"] + "\n";
+                    displayString = displayString + searchResults[property]['raw_code'] + "\n";
+                    displayString = displayString + "--------------------------------------------------\n";
+                    resultNum++;
+                }
+            }
+            theNode = document.createTextNode(displayString);
+            results.appendChild(theNode);
+        }
+    }
 
-    console.log(data);
+    //console.log(data);
 
-    //XHR.open('POST', 'https://ourdatabase.com');
+    XHR.open('POST', 'http://localhost:5000/search');
 
-    //XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    XHR.setRequestHeader('Content-Type', 'application/json');
 
-    //XHR.send(urlEncodedData);
+    XHR.send(JSON.stringify(data));
+
+    //results.addEventListener("")
 }
